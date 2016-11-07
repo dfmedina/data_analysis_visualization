7620c95b0615c54d35cff14f08e5d1120d262ca2
Diego Medina <diego.medina@globant.com>
2016-11-07 11:46:40 -0300

    adding print statement to import.py

--- postgresql/import_data.py
+++ postgresql/import_data.py
@@ -3,9 +3,14 @@ import psycopg2
 conn = psycopg2.connect("host='localhost' port='5432' dbname='dav' user='dba' password='root'")
 cur = conn.cursor()
 f = open(r'C:\Globant\mentoring\WIKI_PRICES.csv', 'r')
-cur.copy_from(f,
-              'dav.data_landing',
-              columns=('ticker', 'date', 'open', 'high', 'low', 'close', 'volume', '"ex-dividend"',
-                       'split_ratio', 'adj_open', 'adj_high', 'adj_low', 'adj_close', 'adj_volume'),
-              sep=',')
+try:
+    print "importing data"  # TODO: add this to log
+    cur.execute('TRUNCATE dav.data_landing;')
+    cur.copy_from(f,
+                  'dav.data_landing',
+                  columns=('ticker', 'date', 'open', 'high', 'low', 'close', 'volume', '"ex-dividend"',
+                           'split_ratio', 'adj_open', 'adj_high', 'adj_low', 'adj_close', 'adj_volume'),
+                  sep=',')
+except psycopg2.DatabaseError:
+    print "Data could not be imported"  # TODO: add this to log
 f.close()
